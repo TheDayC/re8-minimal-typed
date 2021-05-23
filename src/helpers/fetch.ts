813d455a732ed3932c01c5f14140100d0031c1fd
TheDayC <daveconnolly90@gmail.com>
2021-05-23 17:43:38 +0100

    Fetch helper added

---
+++ src/helpers/fetch.ts
@@ -0,0 +1,28 @@
+import { IFetchOptions } from '../types/fetch';
+
+function buildRequest(options: IFetchOptions): RequestInit {
+    const requestInit: RequestInit = {
+        body: options.method === 'POST' ? JSON.stringify(options.body) : undefined,
+        headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+            ...(options.headers || {})
+        },
+        method: options.method || 'GET'
+    };
+
+    return requestInit;
+}
+
+export async function apiFetch<P>(url: string, options: IFetchOptions): Promise<P> {
+    // Initialise request
+    const requestInit = buildRequest(options);
+
+    // Fetch a response from path with request params.
+    const response = await fetch(url, { ...requestInit });
+
+    // Parse json from response and return.
+    const body = await response.json();
+
+    return body;
+}
