726bb7eaf0a27db5a65f944fc36b7cd4cd77a4fe
TheDayC <daveconnolly90@gmail.com>
2021-06-20 15:47:36 +0100

    Poll data feed

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import Health from './components/Health';
 import { getData } from './services/data';
 
 const App: React.FC = () => {
+    const pollingRate = process.env.REACT_APP_POLLING_RATE ? parseInt(process.env.REACT_APP_POLLING_RATE) : 1000;
 
     useEffect(() => {
         getData();
+        setInterval(getData, pollingRate);
     }, []);
 
     return (
