5be934dabfde40b8413f930ab5434b1b4ab089f4
David Connolly <daveconnolly90@gmail.com>
2021-07-20 17:51:30 +0100

    Updated max and current health references (#6)

--- src/services/parsers/game.ts
+++ src/services/parsers/game.ts
@@ -3,8 +3,8 @@ import { IGameState} from '../../store/types/state';
 
 export function parseGameDataResponse(res: unknown): IGameState {
     return {
-        maxHealth: safelyParse(res, 'PlayerMaxHealth', parseAsNumber, null),
-        currentHealth: safelyParse(res, 'PlayerCurrentHealth', parseAsNumber, null),
+        maxHealth: safelyParse(res, 'Player.MaxHP', parseAsNumber, null),
+        currentHealth: safelyParse(res, 'Player.CurrentHP', parseAsNumber, null),
         enemyHealth: safelyParse(res, 'EnemyHealth', parseAsEnemyHealth, null),
         lei: safelyParse(res, 'Lei', parseAsNumber, null),
         rank: safelyParse(res, 'Rank', parseAsNumber, null),
